771b00bc1e2ce75833552ab4a8d541b5da6787d7
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-10-14 23:52:14 +0300

    Add files via upload

--- Тема 7/ConsoleApp1/ConsoleApp1/Program.cs
+++ Тема 7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,143 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Задача 1.Соответствие номера из таблицы Unicode символу 
+
+            //for (int i = 0; i <= 255; i++) 
+
+            //{ 
+
+            //    Console.WriteLine($"Код {i} соотв.символу {(char)i}");    
+
+            //} 
+
+
+
+            //Задача 2.Дан символ.Вывести его код 
+
+            //Console.Write("Введите символ:"); 
+
+            //char ch = char.Parse(Console.ReadLine()); 
+
+            //Console.WriteLine($"Символ {ch} имеет код {(int)ch}"); 
+
+
+
+            //Задача 3.Дан символ.Вывести символ, который в кодовой таблице ASCII следует за этим символом 
+
+            //Console.Write("Введите символ:"); 
+
+            //char ch = char.Parse(Console.ReadLine()); 
+
+            //Console.WriteLine($"символ{(char)(int)ch + 1}"); 
+
+
+
+            //Задача 4.Дан символ.Вывести два символа, стоящие перед ним в кодовой таблице ASCII 
+
+            //Console.Write("Введите символ:"); 
+
+            //char a = Convert.ToChar(Console.ReadLine()); 
+
+            //int b = (int)a; 
+
+            //b -= 1; 
+
+            //int z = (int)a; 
+
+            //z -= 2; 
+
+            //Console.WriteLine($"За символом {a} символ {(char)b} и {(char)z} "); 
+
+            //Console.ReadKey(); 
+
+
+
+            //Задача 5.Дан символ.Вывести два символа, первый из которых предшествует введенному символу в кодовой таблице, а второй – следует после него 
+
+            //Console.Write("Введите символ:"); 
+
+            //char d = Convert.ToChar(Console.ReadLine()); 
+
+            //int b = (int)d; 
+
+            //b -= 1; 
+
+            //int z = (int)d; 
+
+            //z += 1; 
+
+            //Console.WriteLine($"Перед симвоолом {d} символ {(char)b} и После {(char)z} "); 
+
+            //Console.ReadKey(); 
+
+
+
+            //Задача 6.Дано название футбольного клуба.Напечатать его на экране «столбиком» 
+
+            //Console.Write("Введите название клуба:"); 
+
+            //string ch = (Console.ReadLine()); 
+
+            //char[] omas = ch.ToCharArray(); 
+
+            //for (int i = 0; i < omas.Length; i++) 
+
+            //{ 
+
+            //    Console.WriteLine(omas[i]); 
+
+            //} 
+
+
+
+            // Задача 7.Составить программу, которая печатает заданное слово, начиная с последней буквы 
+
+            //Console.Write("Введите слово:"); 
+
+            //string ch = (Console.ReadLine()); 
+
+            //char[] omas = ch.ToCharArray(); 
+
+            //for (int i  = omas.Length-1; i >=0; i--) 
+
+            //{ 
+
+            //    Console.Write(omas[i]); 
+
+            //} 
+
+
+
+
+
+            //Задача 8.Преобразовать символы в строке из строчных в прописные и обратно 
+
+            //Console.Write("Введите строку:"); 
+
+            //string ch = Console.ReadLine(); 
+
+            //char [] omas = ch.ToCharArray(); 
+
+            //for (int i = 0; i < omas.Length; i++) 
+
+            //{ 
+
+            //    Console.Write(char.ToUpper(omas[i])); 
+
+            //} 
+
+            //Console.WriteLine(); 
+
+            //for (int i = 0; i < omas.Length; i++) 
+
+            //{ 
+
+            //    Console.Write(char.ToLower(omas[i])); 
+
+            //} 
+
+            Console.ReadKey();
         }
     }
 }
